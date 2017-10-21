516f8c5de911372cd598da5f4d4fc3a96e47ab8d
nicoleorzan <orzan.nicole@gmail.com>
2017-10-21 12:07:37 +0200

    esercise on optimizing the 'distribute partcle' code added

--- D3-exercise/distribute_particles/distribute_v1.c
+++ D3-exercise/distribute_particles/distribute_v1.c
@@ -1,4 +1,7 @@
 /*
+
+PRIMO STEP: TOLGO LA RADICE QUADRATA NEL CALCOLO DELLA DISTANZA E METTO <RMAX^2
+
  * This file is part of the exercises for the Lectures on 
  *   "Foundations of High Performance Computing"
  * given at 
@@ -23,7 +26,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/resource.h>
+#include <sys/resource.h> //questi 3 sono per il calcolo dei tempi
 #include <sys/times.h>
 #include <time.h>
 #include <math.h>
@@ -34,7 +37,7 @@
  * optimization in loops, both in optimization and in algorithms.
  *
  * Conceptually, it is a loop over a number Np of particles
- * with tha aim of distributing the mass of the particles iver the grid
+ * with tha aim of distributing the mass of the particles over the grid
  * with an appropriate weigth function, so that to have a resulting
  * density field instead of a particles distribution.
  *
@@ -56,14 +59,8 @@
 
 typedef enum {NGP, CIC, TSC} MAS_type;
 
-typedef unsigned long long int ULL;
-
 MAS_type MAS_select;
 double   RMax;
-
-// [change]
-// this variables are now global
-double   size, cell_size, half_size;
 
 
 // =======================================================================
@@ -85,14 +82,14 @@ int    ijk(int, int, int, int);
 // to a cell at distance d
 double MAS(double d)
 {
-  double distance = fabs(d);
+  double distance = fabs(d); //valore assoluto di x
   double result;
   
   if(MAS_select == NGP)
     {
       if (distance < 0.5) result=1.0;
       else if (distance == 0.5) result=0.5;
-      else if (distance>0.5) result=0.0;
+      else if (distance > 0.5) result=0.0;
     }
   
   else if(MAS_select == CIC)
@@ -115,24 +112,28 @@ double get_RMax(void)
   double result;
   
   if(MAS_select == NGP)
-    result = cell_size;
+    result = 0.5;
   
   else if(MAS_select == CIC)
-    result = cell_size;
+    result = 1.0;
 
   else
-    result = 1.5 * cell_size;
+    result = 1.5;
   
   return result;
+
 }
 
 
 
 int ijk(int N, int i, int j, int k)
 {
-  return i * N*N + j * N  + k;
+  int res;
+
+  res = i * (N*N) + j * (N) + k;
+  return res;
 }
-  
+
 
 
 int main(int argc, char **argv)
@@ -164,41 +165,24 @@ int main(int argc, char **argv)
   // get the type of weight function to use
   MAS_select = atoi( *(argv + 3) );
 
-  size = 1.0;
-  cell_size = 1.0 / Ng;
-  half_size = cell_size / 2.0;
-  
   RMax = get_RMax();
 
-  // allocate contiguous memory for grid cells
-  // [change]
-  // you should always check whether the memory allocation has succeeded
-  if( (Grid = (double*)calloc(Ng * Ng * Ng, sizeof(double))) == NULL )
-    {
-      printf("unable to allocate %lu bytes of memory, I have to say good bye it's time to die\n", Ng * Ng * Ng * sizeof(double));
-      exit(1);
-    }
 
+  
   // allocate contiguous memory for particles coordinates
-  // [change]
-  // you should always check whether the memory allocation has succeeded
-  if( (x    = (double*)calloc(Np * 3, sizeof(double))) == NULL)
-    {
-      printf("unable to allocate %lu bytes of memory, I have to say good bye it's time to die\n", Np * 3 * sizeof(double));
-      free(Grid);
-      exit(1);
-    }
+  x    = (double*)calloc(Np * 3, sizeof(double)); //4
   y    = x + Np;
   z    = y + Np;
 
-
+  // allocate contiguous memory for grid cells
+  Grid = (double*)calloc(Ng * Ng * Ng, sizeof(double));
     
   // initialize random number generator
   //srand48(clock());   // change seed at each call
   srand48(997766);    // same seed to reproduce results
 
   // initialize mock coordinates, in the interval [0, 1[
-  printf("initialize coordinates..\n");
+  //printf("initialize coordinates..\n");
   for(i = 0; i < 3*Np; i++)
     x[i] = drand48();
 
@@ -209,94 +193,40 @@ int main(int argc, char **argv)
   // LOOP 0: the worst case
   // ---------------------------------------------------
 
-  printf("\nstart vb run:: \n"
-	 "\t MAS type  : %d\n"
-	 "\t     RMax  : %g\n"
-	 "\t     Grid  : %-3d^3\n"
-	 "\t particles : %-4d\n\n",
-	 
-	 MAS_select, RMax, Ng, Np ); fflush(stdout);
+  
+  printf(" v0 :: "); fflush(stdout);
 
-#ifdef DEBUG
-  // [change]
-  // debug variables
-  int dbg_assignments, *dbg_done;
-  dbg_assignments = 0;
-  dbg_done        = (int*)calloc(Np, sizeof(int));
-#endif
-
+  double dist;
+  double half_size = 0.5 / Ng;
+  
   ctime = 0;
   tstart = TCPU_TIME;
+  double RMax2 = RMax * RMax;
+
   
   // loop over all cells
   for(i = 0; i < Ng; i++)
     for(j = 0; j < Ng; j++)
       for(k = 0; k < Ng; k++)
-	for(p = 0; p < Np; p++)
-	  {
-	    // loop over all particles
-	    double dist = sqrt( pow(x[p] - ((double)i/Ng + half_size), 2) +
-				pow(y[p] - ((double)j/Ng + half_size), 2) +
-				pow(z[p] - ((double)k/Ng + half_size), 2) );
-	    
-	    if(dist < RMax)
-	      {
-		Grid[ijk(Ng, i, j, k)] += MAS(dist / cell_size);
-#ifdef DEBUG		      		  
-		/* printf("p (%g %g %g) %d visits cell %d %d %d" */
-		/*        " (dist is %g | %g | %g | %g)\n", */
-		/*        x[p], y[p], z[p], p, i, j, k, dist, */
-		/*        x[p] - ((double)i/Ng + half_size), */
-		/*        y[p] - ((double)j/Ng + half_size), */
-		/*        z[p] - ((double)k/Ng + half_size) ); */
-		// [change]
-		// keep track about the particles assigned and about the total # of assignments
-		dbg_assignments++;
-		dbg_done[p]++;
-#endif
-	      }
+	{
+	  // loop over all particles
+	  for(p = 0; p < Np; p++)
+	    {
+	      dist =  pow(x[p] - (double)i/Ng + half_size, 2) +
+			   pow(y[p] - (double)j/Ng + half_size, 2) +
+			   pow(z[p]  - (double)k/Ng + half_size, 2) ;
+	      if(dist < RMax2)
+		Grid[ijk(Ng, i, j, k)] += MAS(dist);
+	    }
 	  }
-  
+
+
   ctime += TCPU_TIME - tstart;
 
-#ifdef DEBUG
-  // [change]
-  for(j = 0, i = 0; i < Np; i++)
-    if(dbg_done[i] == 0)
-      j++;
-  
-  if(j > 0)
-    printf("\nWARNING:: %d particles have not be assigned to any cell\n", j);
+  printf("\t%g sec\n", ctime);
 
-  printf("\nrun took %g seconds, %d assignments done\n", ctime, dbg_assignments);
-  free(dbg_done);
-#else
-  printf("\nrun took %g seconds\n", ctime);
-#endif
-
-
-  // [change]
-  // write the grid by column in ascii format, with small significant digits so
-  // that to not much sensitive to errors in summation
-  // ... verification...
-  FILE *file;
-  int   N;
-
-  file = fopen("grid_v0_rd.dat", "w");
-  
-  N = Ng *Ng;
-  for(i = 0; i < N; i++)
-    {
-      k = i*Ng;
-      for(j = 0; j< Ng; j++)
-	fprintf(file, "%9.6g ", Grid[k + i]);
-      fprintf(file, "\n");
-    }
-  
-  fclose(file);
-  
   free(Grid);  
-  free(x);
-
+  free(x);    
+  
   return 0;
 }
