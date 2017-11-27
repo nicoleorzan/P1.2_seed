a4be4e1ca0a837ade6116ca1a4db6d66d139642b
nicoleorzan <orzan.nicole@gmail.com>
2017-11-27 12:29:24 +0100

    some errors on the eercises fixed

--- lectures/ParallelProgramming/hands-on/CUDA/multimatr_version1.cu
+++ lectures/ParallelProgramming/hands-on/CUDA/multimatr_version1.cu
@@ -1,26 +1,26 @@
 #include<stdio.h>
 #include<assert.h>
 
-#define N 10 //0000
-#define NUM_THREADS 512
+#define N 4096
+//#define NUM_THREADS 512
+#define BK_SIZE 512
 
-__global__ void matmult( double * d_A, double * d_B, double * d_C, int SIZE )
+__global__ void matmult( double * d_A, double * d_B, double * d_C)
 {
-int i = threadIdx.x + (blockIdx.x * blockDim.x);
-int j = threadIdx.y + (blockIdx.y * blockDim.y);
+  int i = threadIdx.x + (blockIdx.x * blockDim.x);
+  int j = threadIdx.y + (blockIdx.y * blockDim.y);
+  for (i=0; i<N; i++){ 
+    for (j=0; j<N; j++){
+      for (int k=0; k<N; k++){
+	d_C[j+i*N]+=d_A[i*N+k]*d_B[k*N+j];
+      }
+    }
+  }
+  
+}
 
-for (i=0; i<SIZE; i++){
-for (j=0; j<SIZE; j++){
-for (int k=0; k<SIZE; k++){
-d_C[j+i*SIZE]+=d_A[i*SIZE+k]*d_B[k*SIZE+j];
-}
-}
-}
-
-}
-
-void PRINT_MAT(int N, int M, double * matr){
-  for(int j = 0; j < N; j++ ){
+void PRINT_MAT(int P, int M, double * matr){
+  for(int j = 0; j < P; j++ ){
     for(int i = 0; i < M; i++ ){
       printf("%f ",matr[i+j*M]);
     }
@@ -31,45 +31,47 @@ void PRINT_MAT(int N, int M, double * matr){
 }
 
 int main(){
+  
+  double h_A ,h_B, h_C;
+  double d_A ,d_B, d_C;
+  size_t matsize = N * N * sizeof(double); //long integer
 
-double h_A ,h_B, h_C;
-double d_A ,d_B, d_C;
-size_t matsize = N * N * sizeof(double); //long integer
+  h_A = (double *) malloc( matsize );
+  h_B = (double *) malloc( matsize );
+  h_C = (double *) malloc( matsize );
+  
+  cudaMalloc((void**), &d_A, matsize );
+  cudaMalloc((void**), &d_B, matsize );
+  cudaMalloc((void**), &d_C, matsize );
+  
+  for(int i=0;i<N;i++){
+    h_A[i]=(double *)i;
+    h_B[i]=(double *)i;
+    h_C[i]=0.;
+  }
+  
+  PRINT_MAT(N,N,h_A);
+  PRINT_MAT(N,N,h_B);
+  
+  cudaMemcpy( d_A, h_A, matsize, cudaMemcpyHostToDevice );
+  cudaMemcpy( d_B, h_B, matsize, cudaMemcpyHostToDevice );
+  cudaMemcpy( d_C, h_C, matsize, cudaMemcpyHostToDevice );
 
-h_A = (double *) malloc( matsize );
-h_B = (double *) malloc( matsize );
-h_C = (double *) malloc( matsize );
-
-cudaMalloc((void**), &d_A, matsize );
-cudaMalloc((void**), &d_B, matsize );
-cudaMalloc((void**), &d_C, matsize );
-
-for(int i=0;i<N;i++){
-h_A[i]=(double *)i;
-h_B[i]=(double *)i;
-h_C[i]=0.;
-}
-
-PRINT_MAT(N,N,h_A);
-PRINT_MAT(N,N,h_B);
-
-cudaMemcpy( d_A, h_A, matsize, cudaMemcpyHostToDevice );
-cudaMemcpy( d_B, h_B, matsize, cudaMemcpyHostToDevice );
-cudaMemcpy( d_C, h_C, matsize, cudaMemcpyHostToDevice );
-
-matmult<<< ((N*N)+NUM_THREADS)/NUM_THREADS , NUM_THREADS >>>( d_A, d_B, d_C, N )
-cudaMemcpy( h_C, d_C, matsize, cudaMemcpyDeviceToHost );
-
-PRINT_MAT(N,N,h_C);
-
-free(h_A);
-free(h_B);
-free(h_C);
-
-cudaFree(d_A);
-cudaFree(d_B);
-cudaFree(d_C);
-
-return 0;
-
+  dim3 blockDim(BK_SIZE, BK_SIZE);
+  dim3 gridDim(N/BK_SIZE, N/BK_SIZE);
+  matmult<<< gridDim, blockDim >>>( d_A, d_B, d_C);
+  cudaMemcpy( h_C, d_C, matsize, cudaMemcpyDeviceToHost );
+  
+  PRINT_MAT(N,N,h_C);
+  
+  free(h_A);
+  free(h_B);
+  free(h_C);
+  
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+  
+  return 0;
+  
 }
--- lectures/ParallelProgramming/hands-on/CUDA/multimatr_version2.cu
+++ lectures/ParallelProgramming/hands-on/CUDA/multimatr_version2.cu
@@ -1,34 +1,48 @@
 #include<stdio.h>
 #include<assert.h>
 
-#define N 10 //0000
-#define NUM_THREADS 512
+#define N 4096
+//#define NUM_THREADS 512
+#define BK_SIZE 512
 
-__global__ void matmult( double * d_A, double * d_B, double * d_C, int SIZE )
+__global__ void matmult( double * d_A, double * d_B, double * d_C )
 {
-__shared__ double Arow[SIZE];
+  __shared__ double Arow[N];
+  
+  for (int i=0;i<N;i++){
+    Arow[i]=d_A[i+N*blockIdx.x];
+  }
+  
+  for (int i=0; i<N; i++){
+    for (int j=0; j<N; j++){
+      d_C[i]+=Arow[j]*d_B[j*N+i];
+    }
+  }
 
-//int idx = threadIdx.x + (blockIdx.x * blockDim.x);
-//int idy = threadIdx.y + (blockIdx.y * blockDim.y);
+  /*
+    idx = threadIdx.x
+    idy = blockIdx.y
+    while(idy<N){
+    Arow[idy]=d_A[idy+N*idx];
+    idy+=blockDim.x
+    }
+    idy+= threadIdx.y
 
-for (int i=0;i<SIZE;i++){
-Arow[i]=d_A[i+SIZE*blockIdx.x];
+    for (int k=0; k<N; k++){
+    d_C[idx*N+idy]+=Arow[k]*d_B[k*N+idy];
+    idy+=threadDim.x;
+    }
+   */
+  
+  __syncthreads();
+  
 }
 
-for (int i=0; i<SIZE; i++){
-for (int j=0; j<SIZE; j++){
-d_C[i]+=Arow[j]*d_B[j*SIZE+i];
-}
-}
-__syncthreads();
-
-}
-
-void PRINT_MAT(int N, int M, double * matr){
-  for(int j = 0; j < N; j++ ){
+void PRINT_MAT(int P, int M, double * matr){
+  for(int j = 0; j < P; j++ ){
     for(int i = 0; i < M; i++ ){
       printf("%f ",matr[i+j*M]);
-    }
+     }
     printf("\n");
   }
 }
@@ -36,45 +50,47 @@ void PRINT_MAT(int N, int M, double * matr){
 }
 
 int main(){
+  
+  double h_A ,h_B, h_C;
+  double d_A ,d_B, d_C;
+  size_t matsize = N * N * sizeof(double); //long integer
+  
+  h_A = (double *) malloc( matsize );
+  h_B = (double *) malloc( matsize );
+  h_C = (double *) malloc( matsize );
+  
+  cudaMalloc((void**), &d_A, matsize );
+  cudaMalloc((void**), &d_B, matsize );
+  cudaMalloc((void**), &d_C, matsize );
+  
+  for(int i=0;i<N;i++){
+    h_A[i]=(double *)i;
+    h_B[i]=(double *)i;
+    h_C[i]=0.;
+  }
+  
+  PRINT_MAT(N,N,h_A);
+  PRINT_MAT(N,N,h_B);
+  
+  cudaMemcpy( d_A, h_A, matsize, cudaMemcpyHostToDevice );
+  cudaMemcpy( d_B, h_B, matsize, cudaMemcpyHostToDevice );
+  cudaMemcpy( d_C, h_C, matsize, cudaMemcpyHostToDevice );
 
-double h_A ,h_B, h_C;
-double d_A ,d_B, d_C;
-size_t matsize = N * N * sizeof(double); //long integer
-
-h_A = (double *) malloc( matsize );
-h_B = (double *) malloc( matsize );
-h_C = (double *) malloc( matsize );
-
-cudaMalloc((void**), &d_A, matsize );
-cudaMalloc((void**), &d_B, matsize );
-cudaMalloc((void**), &d_C, matsize );
-
-for(int i=0;i<N;i++){
-h_A[i]=(double *)i;
-h_B[i]=(double *)i;
-h_C[i]=0.;
-}
-
-PRINT_MAT(N,N,h_A);
-PRINT_MAT(N,N,h_B);
-
-cudaMemcpy( d_A, h_A, matsize, cudaMemcpyHostToDevice );
-cudaMemcpy( d_B, h_B, matsize, cudaMemcpyHostToDevice );
-cudaMemcpy( d_C, h_C, matsize, cudaMemcpyHostToDevice );
-
-matmult<<< ((N*N)+NUM_THREADS)/NUM_THREADS , NUM_THREADS >>>( d_A, d_B, d_C, N )
-cudaMemcpy( h_C, d_C, matsize, cudaMemcpyDeviceToHost );
-
-PRINT_MAT(N,N,h_C);
-
-free(h_A);
-free(h_B);
-free(h_C);
-
-cudaFree(d_A);
-cudaFree(d_B);
-cudaFree(d_C);
-
-return 0;
-
+  dim3 blockDim(BK_SIZE, BK_SIZE);
+  dim3 gridDim(N/BK_SIZE, N/BK_SIZE);
+  matmult<<< gridDim, blockDim >>>( d_A, d_B, d_C);
+  cudaMemcpy( h_C, d_C, matsize, cudaMemcpyDeviceToHost );
+  
+  PRINT_MAT(N,N,h_C);
+  
+  free(h_A);
+  free(h_B);
+  free(h_C);
+  
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+  
+  return 0;
+  
 }
